71018884feb9da5aece4a0f8e69486df442a8647
Matthew <murphy1022@gmail.com>
2016-10-15 14:14:45 -0700

    Create the list interface

---
+++ src/main/c/com/micromouse/utility/list/list.c
@@ -0,0 +1,61 @@
+#include "list.h"
+
+List * InitializeList(Cell * data) {
+	// TODO: Implement
+	return NULL;
+}
+
+void ListDestructor(List * list) {
+	// TODO: Implement
+}
+
+Cell * front(List * list) {
+	// TODO: Implement
+	return NULL;
+}
+
+Cell * back(List * list) {
+	// TODO: Implement
+	return NULL;
+}
+
+void PushBack(Cell * data) {
+	// TODO: Implement
+}
+
+void Append(Cell * data) {
+	// TODO: Implement
+}
+
+Cell * get(int index) {
+	// TODO: Implement
+	return NULL;
+}
+
+bool in(Cell * data) {
+	// TODO: Implement
+	return false;
+}
+
+int length(List * list) {
+	// TODO: Implement
+	return 0;
+}
+
+bool empty(List * list) {
+	// TODO: Implement
+	return false;
+}
+
+void clear(List * list) {
+	// TODO: Implement
+}
+
+void erase(Cell * data) {
+	// TODO: Implement
+}
+
+List * reverse(List * list) {
+	// TODO: Implement
+	return NULL;
+}
---
+++ src/main/c/com/micromouse/utility/list/list.h
@@ -0,0 +1,57 @@
+#ifndef MICROMOUSE_UTILITY_LIST_LIST_H_
+#define MICROMOUSE_UTILITY_LIST_LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "src/main/c/com/micromouse/maze/cell.h"
+
+// A linked list used to represent a list of cells.
+typedef struct List {
+	struct List * prev_;
+	struct List * next_;
+
+	// Data for the list.
+	Cell * data_;
+} List;
+
+// Create a node with the given data.
+List * InitializeList(Cell * data);
+
+// Free all of the memory associated with the list. Goes through all of the 
+// nodes after this node in the list and frees their memory.
+void ListDestructor(List * list);
+
+// Returns the first element of the list.
+Cell * front(List * list);
+
+// Returns the last element of the list.
+Cell * back(List * list);
+
+// Pushes the element onto the end of the list.
+void PushBack(Cell * data);
+
+// Appends the element to the front of the list.
+void Append(Cell * data);
+
+// Gets the element at index in the list. Return NULL if not found.
+Cell * get(int index);
+
+// Returns if the element is in the list.
+bool in(Cell * data);
+
+// Returns the length of the list.
+int length(List * list);
+
+// Returns if the list is empty.
+bool empty(List * list);
+
+// Clears the list, does not free the memory associated with the list.
+void clear(List * list);
+
+// Erases the first element to match the data in the list.
+void erase(Cell * data);
+
+// Reverses the list.
+List * reverse(List * list);
+
+#endif  // MICROMOUSE_UTILITY_LIST_LIST_H_
