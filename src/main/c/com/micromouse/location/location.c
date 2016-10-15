e89acffbba50939096c93003ac36a82c24586e2e
Matthew <murphy1022@gmail.com>
2016-10-15 09:12:55 -0700

    Add function stubs to location

--- src/main/c/com/micromouse/location/location.c
+++ src/main/c/com/micromouse/location/location.c
@@ -1,1 +1,10 @@
 #include "location.h"
+
+Location * InitializeLocation(int x, int y) {
+	// TODO: Implement
+	return NULL;
+}
+
+void LocationDestructor(Location * locations) {
+	// TODO: Implement
+}
--- src/main/c/com/micromouse/location/location.h
+++ src/main/c/com/micromouse/location/location.h
@@ -1,11 +1,19 @@
 #ifndef MICROMOUSE_LOCATION_LOCATION_H_
 #define MICROMOUSE_LOCATION_LOCATION_H_
 
+#include <stdio.h>
+
 // A struct for representing a location
 // A location has a x and y position  
 typedef struct Location {
     int x;
     int y;
 } Location;
+
+// Returns a pointer to a new location at the given x and y coordinates.
+Location * InitializeLocation(int x, int y);
+
+// Frees up all of the memory associated with the given location.
+void LocationDestructor(Location * location);
 
 #endif  // MICROMOUSE_LOCATION_LOCATION_H_
