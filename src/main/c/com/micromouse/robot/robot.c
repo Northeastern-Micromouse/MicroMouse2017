50f33636331b542e9ccee23fe44cb769ffe0ccc0
Matthew <murphy1022@gmail.com>
2016-10-15 12:57:18 -0700

    Implemnt Robot constructor and destructor and add testsy

--- src/main/c/com/micromouse/robot/robot.c
+++ src/main/c/com/micromouse/robot/robot.c
@@ -1,10 +1,45 @@
 #include "robot.h"
 
-Robot * InitializeRobot(Location location) {
-	// TODO: Implement
-	return NULL;
+Robot * InitializeRobot(Location * location) {
+	// Create the struct for winslow.
+	Robot * winslow;
+
+	// Request memory from the heap.
+	winslow = (Robot *)malloc(sizeof(Robot));
+
+	// Check if the memory is avaliable.
+	if (winslow == 0) {
+		printf("Error out of memory. Unable to create winslow"
+		" at location x: %d, y: %d.\n", location->x, location->y);
+		return NULL;
+		return NULL;
+	}
+
+	// Check that the location is a valid location.
+	if (location->x < 0 || location->y < 0) {
+		printf("Invalid inputs when creating winslow. "
+			"The location is: x: %d, y: %d. \n", location->x, location->y);
+	}
+
+	// Set the values.
+	winslow->location_ = location;
+	for (int row = 0; row < 16; row++) {
+		for (int col = 0; col < 16; col++) {
+			winslow->maze_[row][col] = InitializeCell(row, col);
+		}
+	}
+	return winslow;
 }
 
-void RobotDestrutor(Robot * winslow) {
-	// TODO: Implement
+void RobotDestructor(Robot * winslow) {
+	// Call the destructor on all of its contents.
+	LocationDestructor(winslow->location_);
+	for (int row = 0; row < 16; row++) {
+		for (int col = 0; col < 16; col++) {
+			CellDestructor(winslow->maze_[row][col]);
+		}
+	}
+
+	// Free up the memory for the Robot
+	free(winslow);
 }
