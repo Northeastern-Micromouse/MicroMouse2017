fd01fa4cbc9dcd2ac7686fd9b148d43f3c00de28
Matthew <murphy1022@gmail.com>
2016-10-19 20:56:39 -0700

    Start defining the maze exploration function and the helpers

--- src/main/c/com/micromouse/robot/robot.h
+++ src/main/c/com/micromouse/robot/robot.h
@@ -1,8 +1,9 @@
 #ifndef MICROMOUSE_ROBOT_ROBOT_H_
 #define MICROMOUSE_ROBOT_ROBOT_H_
 
-#include "src/main/c/com/micromouse/maze/cell.h"
+#include "src/main/c/com/micromouse/hardware/hardware.h"
 #include "src/main/c/com/micromouse/location/location.h"
+#include "src/main/c/com/micromouse/maze/cell.h"
 
 typedef struct Robot {
 	Location * location_;
@@ -14,5 +15,11 @@ Robot * InitializeRobot(Location * location);
 
 // Frees all of the memory associated with the given robot.
 void RobotDestructor(Robot * winslow);
+
+// Explores the maze.
+void ExploreMaze(Robot * winslow);
+
+// Updates the maze in the robot with the sensor information/
+void UpdateMaze(Robot * winslow, SR * values);
 
 #endif  // MICROMOUSE_ROBOT_ROBOT_H_
