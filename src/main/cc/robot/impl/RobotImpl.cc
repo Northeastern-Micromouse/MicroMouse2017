5a20ea3c84ddfdd550a00147fdd31bf0ba268298
Matthew Murphy <murphy1022@gmail.com>
2017-01-14 13:29:20 -0500

    Small fixes (#47)

--- src/main/cc/robot/impl/RobotImpl.cc
+++ src/main/cc/robot/impl/RobotImpl.cc
@@ -12,42 +12,42 @@ void RobotImpl::StartExploration() {
   if (curr_loc_.x() != 0 && curr_loc_.y() != 0) {
     ReturnToStart();
   }
-  Cell * curr_cell;
-  std::vector<Cell *> neighbors;
+  Cell* curr_cell;
+  std::vector<Cell*> neighbors;
   Cell::RelativeDirection prev_move = Cell::RelativeDirection::NONE;
 
   // Visit the first cell.
   VisitCurrentCell();
 
-//  // While there are still possible moves to go.
-//  while (!stack_.empty()) {
-//    curr_cell = stack_.top();
-//    stack_.pop();
-//    // If it is not visited we need to visit it.
-//    if (!curr_cell->isVisited()) {
-//      prev_move = GetDirection(curr_cell);
-//      // Move the robot to the cell.
-//      Move(prev_move);
-//      // Visit the cell.
-//      curr_cell->VisitCell();
-//      neighbors = GetNeighbors();
-//      // If it is empty we cannot go forward any more, so move back.
-//      if (!neighbors.empty()) {
-//        // Loop through all of its possible neighbors.
-//        for (Cell *neighbor : neighbors) {
-//          // If they are not visited then push them onto the stack to visit later
-//          if (!neighbor->isVisited()) {
-//            stack_.push(neighbor);
-//          }
-//        }
-//      } else {
-//        GoBack(prev_move);
-//      }
-//    } else {
-//      // Otherwise we have to move back
-//      GoBack(prev_move);
-//    }
-//  }
+  // While there are still possible moves to go.
+  while (!stack_.empty()) {
+    curr_cell = stack_.top();
+    stack_.pop();
+    // If it is not visited we need to visit it.
+    if (!curr_cell->isVisited()) {
+      prev_move = GetDirection(curr_cell);
+      // Move the robot to the cell.
+      Move(prev_move);
+      // Visit the cell.
+      curr_cell->VisitCell();
+      neighbors = GetNeighbors();
+      // If it is empty we cannot go forward any more, so move back.
+      if (!neighbors.empty()) {
+        // Loop through all of its possible neighbors.
+        for (Cell* neighbor : neighbors) {
+          // If they are not visited then push them onto the stack to visit later
+          if (!neighbor->isVisited()) {
+            stack_.push(neighbor);
+          }
+        }
+      } else {
+        GoBack(prev_move);
+      }
+    } else {
+      // Otherwise we have to move back
+      GoBack(prev_move);
+    }
+  }
 }
 
 void RobotImpl::GoToGoal() {
