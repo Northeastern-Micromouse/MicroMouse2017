c98ed68995699231542ce256df0e73168701f105
Matthew Murphy <murphy1022@gmail.com>
2017-02-08 19:21:45 -0500

    Actually get c++ to build and compile (#78)

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "src/main/cc/robot/impl/RobotImpl.h"
 
 int main() {
   std::cout << "Starting the program" << std::endl;
+  RobotImpl winslow = RobotImpl();
 
   return 0;
 }
