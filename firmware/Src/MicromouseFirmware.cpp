71e4e6e0ac2aa5b39229dc31cd11840a8b53f677
lichto2005 <lichto2005@gmail.com>
2017-04-07 04:56:33 -0400

    lots of Robot functions!!!!!!!!111!oneoneone (#118)

--- firmware/Src/MicromouseFirmware.cpp
+++ firmware/Src/MicromouseFirmware.cpp
@@ -28,17 +28,30 @@ int main(void) {
     pal::Adc adc1(hadc1, 2);
     pal::Adc adc2(hadc2, 3);
     pal::Adc adc3(hadc3, 1);
+    pal::I2C i2c(&hi2c1, 10000);
+    pal::Gpio left_step = pal::Gpio(S_MOTOR_B_STEP_GPIO_Port, S_MOTOR_B_STEP_Pin);
+    pal::Gpio left_dir = pal::Gpio(S_MOTOR_B_DIR_GPIO_Port, S_MOTOR_B_DIR_Pin);
+    pal::Gpio right_step = pal::Gpio(S_MOTOR_A_STEP_GPIO_Port, S_MOTOR_A_STEP_Pin);
+    pal::Gpio right_dir = pal::Gpio(S_MOTOR_A_DIR_GPIO_Port, S_MOTOR_A_DIR_Pin);
+    pal::Tim drive_tick = pal::Tim(&htim1);
+    
     phil::Distance distA(adc1, 5, 0);
     phil::Distance distB(adc2, 6, 0);
-    phil::Reflectance reflA(adc1, 7, 1);
-    phil::Reflectance reflB(adc2, 8, 1);
-    phil::Reflectance reflC(adc3, 4, 0);
+    phil::Reflectance reflA(adc1, 7, 1, 0);
+    phil::Reflectance reflB(adc2, 8, 1, 1);
+    phil::Reflectance reflC(adc3, 4, 0, 2);
+    phil::IMU imu(&i2c, BNO055_ADDRESS_A);
+    imu.Initialize();
+    phil::DriveSystem drive_system(&left_step, &left_dir, &right_step, &right_dir, &drive_tick, 1, 1, 1);
+    
+    al::Robot robot(&drive_system, &imu, &distB, &distA, &reflA, &reflB, &reflC);
+    
     printf("Entered main program loop\n");
     while (1) {
-        float distA_out = distA.GetDistance(); // RIGHT
-        //float distB_out = distB.GetDistance(); // LEFT
-        //float reflA_out = reflA.GetDistance(); // LEFT
-        //float reflB_out = reflB.GetDistance(); // RIGHT
-        //float reflC_out = reflC.GetDistance(); // CENTER
+        //float offset_h = robot.GetHorizontalLocation(true, true);
+        //float offset_v = robot.GetVerticalLocation();
+        //float dist = reflC.GetDistance();
+        float left_dist = distB.GetDistance();
+        float right_dist = distA.GetDistance();
     }
   }
